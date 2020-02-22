97f4e28382cc75c7f064d9364a856a1bd8b76133
l(~._.~)cf <lamine1905@gmail.com>
2020-02-22 20:41:20 +0300

    modif

--- crawler.js
+++ crawler.js
@@ -1,32 +1,45 @@
-const puppeteer = require('puppeteer');
-const chalk = require('chalk');
-const { msToMin } = require('./utils');
-const url = 'https://frontendmasters.com';
+const puppeteer = require("puppeteer");
+const chalk = require("chalk");
+const { msToMin } = require("./utils");
+const url = "https://frontendmasters.com";
 const SECONDES = 1000;
 let stopInterval;
 
+const userAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36";
+
 module.exports = async ({ user, pass, courses, id }) => {
-  console.log(chalk.green('You are using frontendmaster-downloader \n'));
-  console.log(chalk.green('Try the login ... \n'));
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  console.log(chalk.green("You are using frontendmaster-downloader \n"));
+  console.log(chalk.green("Try the login ... \n"));
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false
+  });
   const page = await browser.newPage();
-  await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-  );
-  await page.goto(url + '/login');
+  await page.setUserAgent(userAgent);
+  const courseTOC = await getCourseTOC()
+  await page.goto(url + "/login");
 
   await page.waitFor(2 * SECONDES);
 
-  await page.waitForSelector('#username');
-  const username = await page.$('#username');
+  await page.waitForSelector("#username");
+  const username = await page.$("#username");
   await username.type(user);
-  const password = await page.$('#password');
+  const password = await page.$("#password");
   await password.type(pass);
-  const button = await page.$('button');
-  await button.click();
-  console.log(chalk.green(user + ' logged \n'));
-  console.log(chalk.green('First scrape all the links... \n'));
-  let selector = '.title a';
+  const button = await page.$("button");
+  //await button.click();
+  await Promise.all([
+    page.click("button"),
+    page.waitForNavigation({ waitUntil: "networkidle0" })
+  ]);
+
+  console.log(chalk.green(user + " logged \n"));
+  console.log(chalk.green("First scrape all the links... \n"));
+  let selector = ".title a";
+
+  await page.goto(url + "/courses");
+
   await page.waitForSelector(selector);
   const obj = {
     selector,
@@ -41,12 +54,12 @@ module.exports = async ({ user, pass, courses, id }) => {
         return `${anchor.href}`;
       })
       .filter(text => {
-        return text.split('/')[4] === obj.courses;
+        return text.split("/")[4] === obj.courses;
       })
       .pop();
   }, obj);
   await page.goto(link);
-  selector = '.LessonListItem a';
+  selector = ".LessonListItem a";
   await page.waitForSelector(selector);
   const links = await page.evaluate(selector => {
     const anchors = Array.from(document.querySelectorAll(selector));
@@ -69,7 +82,7 @@ module.exports = async ({ user, pass, courses, id }) => {
     const index = useLink.index;
     const link = useLink.link;
     await page.goto(link);
-    selector = 'video';
+    selector = "video";
 
     await page.waitFor(8 * SECONDES);
     const videoLink = await page.evaluate(selector => {
@@ -80,14 +93,14 @@ module.exports = async ({ user, pass, courses, id }) => {
     const fileName =
       `${index + 1}-` +
       link
-        .split('/')
+        .split("/")
         .filter(str => str.length)
         .pop() +
-      '.webm';
+      ".webm";
     try {
       return [{ fileName, videoLink }];
     } catch (err) {
-      console.log('ERROR', err);
+      console.log("ERROR", err);
     }
   } else {
     finalLinks = await getLinks(newLinks);
@@ -95,37 +108,41 @@ module.exports = async ({ user, pass, courses, id }) => {
   }
 
   async function getLinks(newLinks) {
-    for (const templink of newLinks) {
-      console.log(chalk.yellow('scraping', templink.link + '\n'));
+    for (const templink of newLinks.slice(0, 3)) {
+      console.log(chalk.yellow("scraping :", templink.link + "\n"));
       const { index, link } = templink;
       try {
         await page.goto(link);
       } catch (err) {
         //console.log("erreur, err");
       }
-      const selector = 'video';
+      const selector = "video";
 
       await page.waitFor(8 * SECONDES);
+
+      //page1.on('console', msg => console.log(msg.text()));
       let videoLink = await page
         .evaluate(selector => {
-          const video = Array.from(document.querySelectorAll(selector)).pop();
+          const videos = document.querySelectorAll(selector);
+          const video = Array.from(videos).pop();
           return video.src;
         }, selector)
         .catch(err => {
           //console.log(err);
-          return 'retry';
+          return "retry";
         });
       //console.log("video link fetched", videoLink);
+      // console.log(page1.url(), videoLink);
 
-      if (videoLink === 'retry' || !videoLink.length) {
-        console.log(chalk.red('You have reached maximum request limit \n'));
-        console.log(chalk.blue('Sleeping for 15 minutes \n'));
+      if (videoLink === "retry" || !videoLink.length) {
+        console.log(chalk.red("You have reached maximum request limit \n"));
+        console.log(chalk.blue("Sleeping for 15 minutes \n"));
         await timeout(60 * SECONDES * 15);
         clearInterval(stopInterval);
-        console.log(chalk.green('End waiting scraping continues !!!! \n'));
+        console.log(chalk.green("End waiting scraping continues !!!! \n"));
         const { index, link } = templink;
         await page.goto(link);
-        const selector = 'video';
+        const selector = "video";
 
         await page.waitFor(8 * SECONDES);
         videoLink = await page.evaluate(selector => {
@@ -137,10 +154,10 @@ module.exports = async ({ user, pass, courses, id }) => {
       const fileName =
         `${index + 1}-` +
         link
-          .split('/')
+          .split("/")
           .filter(str => str.length)
           .pop() +
-        '.webm';
+        ".webm";
       finalLinks.push({ fileName, videoLink });
     }
     return finalLinks;
@@ -158,6 +175,44 @@ function interval(totalTime, intervalTime) {
   function loggeRemainingTime() {
     remainTime = remainTime - intervalTime;
     let time = msToMin(remainTime);
-    console.log(chalk.blue(time + 'min remaining \n'));
+    console.log(chalk.blue(time + "min remaining \n"));
   }
 }
+
+async function getCourseTOC(courseId) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent);
+  //page.setDefaultNavigationTimeout(0)
+  await page.goto(`${url}/courses/${courseId}`);
+  //await page.waitForNavigation({ waitUntil: "networkidle0" });
+  const title = await page.title();
+  console.log(title);
+  const lessonGroups = ".lessongroup";
+ // await page.waitForSelector(lessonGroups);
+ const courseTOC = await page.evaluate(selector => {
+    let rank = 0;
+    return Array.from(document.querySelectorAll(selector)).map((node, idx) => {
+      const chapterTitle = node.textContent;
+      const chapterLessons = document
+        .querySelector(`${selector}:nth-child(${2 * (idx + 1)}) + .LessonList`)
+        .querySelectorAll(".LessonListItem > a");
+      return {
+        [chapterTitle]: Array.from(chapterLessons).map(link => ({
+          title: link.querySelector(".heading h3").textContent,
+          url: link.getAttribute("href"),
+          rank: ++rank
+        })),
+        index : idx + 1
+      };
+    });
+  }, lessonGroups);
+  await browser.close();
+  return courseTOC;
+}
+
+//getCourseTOC("programming-language");
+require('fs').mkdir('hello/', {recursive: true},(err) =>  {
+  if (err) throw err;
+  console.log('directory created');
+})
